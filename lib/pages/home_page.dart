5da5f9895c23fbdd00d476c3d2d55150a90c7211
Mohammed Farag <mohammed.farag.abuzaid@gmail.com>
2026-05-17 17:26:22 +0300

    colors added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:tf_union/constants/tfcolors.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,16 +13,32 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: tfcolors.scaffoldBg,
       body: ListView(
         scrollDirection: Axis.vertical,
         children: [
-          // the main section
+          // Home
           Container(
-            height: 500,
+            height: 1000,
+            width: double.maxFinite,
+          
+
+          ),
+          // About
+          Container(
+            height: 1000,
             width: double.maxFinite,
             color: Colors.blue,
+          ),
+          // Projects
+          Container(
+            height: 1000,
+            width: double.maxFinite,
 
-          )
+
+          ),
+
+
         ],
       )
     );
