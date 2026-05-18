d841f9a7bea1384bc9dddd05fd3bcecd34cc7de8
Mohammed Farag <mohammed.farag.abuzaid@gmail.com>
2026-05-18 18:31:44 +0300

    started at the hero section

---
+++ lib/constants/variables.dart
@@ -0,0 +1,1 @@
+const double MobileWidth = 700;
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:tf_union/constants/tfcolors.dart';
 import 'package:tf_union/constants/nav_items.dart';
+import 'package:tf_union/constants/variables.dart';
 import 'package:tf_union/widgets/header_desktop.dart';
 import 'package:tf_union/widgets/header_mobile.dart';
 import 'package:tf_union/widgets/logo.dart';
@@ -18,39 +19,58 @@ class _HomePageState extends State<HomePage> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      backgroundColor: TFColors.scaffoldBg,
-      drawer: const DrawerMobile(),
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        children: [ 
-          // header
-          HeaderMobile(
-            onLogoTap: () {
-              
-            },
-            onMenuTap: () {
-              scaffoldKey.currentState?.openDrawer();
-            },
-          ),
-          // About
-          Container(
-            height: 700,
-            width: double.maxFinite,
-            color: TFColors.bglight1,
-          ),
-          // Projects
-          Container(
-            height: 1000,
-            width: double.maxFinite,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          key: scaffoldKey,
+          backgroundColor: TFColors.bglight1,
+          drawer: constraints.maxWidth > MobileWidth?null: const DrawerMobile(),
+          body: ListView(
+            scrollDirection: Axis.vertical,
+            children: [ 
+              // header
+              if (constraints.maxWidth > MobileWidth)
+                const HeaderDesktop(),
+              if (constraints.maxWidth <= MobileWidth)
+                HeaderMobile(
+                onLogoTap: () {},
+                onMenuTap: () {
+                  scaffoldKey.currentState?.openDrawer();
+                },
+              ),
 
+              // Hero
+              Container(
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+                        Text('A None Profit Organization, made Specially for the standout students in Egypt'),
+                        ElevatedButton(onPressed: (){}, child: const Text('Join if you are'))
+                      ],
+                    ),
+                    Image.asset('assets/DarkLogo.png',),
+                  ],
+                ),
 
-          ),
-
-
-        ],
-      )
+              ),
+              // About
+              Container(
+                height: 650,
+                width: double.maxFinite,
+                color: TFColors.scaffoldBg,
+              ),
+              // Projects
+              Container(
+                height: 1000,
+                width: double.maxFinite,
+        ),
+        
+        
+            ],
+          )
+        );
+      }
     );
   }
 }
