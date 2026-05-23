4b7e27460e5b73fcf31f55dd1fb036161cf35ffe
Mohammed Farag <mohammed.farag.abuzaid@gmail.com>
2026-05-23 17:31:42 +0300

    first registeration UI page finished

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,4 +1,6 @@
+import 'package:ez_email_field/ez_email_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_intl_phone_field/flutter_intl_phone_field.dart';
 import 'package:tf_union/constants/tfcolors.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -13,9 +15,11 @@ class _RegisterPageState extends State<RegisterPage> {
 
   final firstName = TextEditingController();
   final lastName = TextEditingController();
-  final birthDate = TextEditingController();
   final email = TextEditingController();
-
+  final TextEditingController _birthDate =TextEditingController();
+  final phone = TextEditingController();
+  final personalEmail = TextEditingController();
+  String? _selectedValue;
 
   @override
   Widget build(BuildContext context) {
@@ -28,19 +32,121 @@ class _RegisterPageState extends State<RegisterPage> {
             title: Text('Account'),
             content: Container(
               child: Column(children: [
-                TextFormField(
-                  controller: firstName,
-                  decoration: InputDecoration(labelText: 'First Name'),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: firstName,
+                    decoration: InputDecoration(
+                      labelText: 'First Name',
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey)
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide( color: Colors.blue)
+                      ),
+                    ),
+                  
+                  ),
                 ),
-                TextFormField(
-                  controller: lastName,
-                  decoration: InputDecoration(labelText: 'Last Name'),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: lastName,
+                    decoration: InputDecoration(
+                      labelText: 'Last Name',
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey)
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide( color: Colors.blue)
+                      ),
+                    ),
+                  ),
                 ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Birth Date',
-                    prefixIcon: Icon(Icons.calendar_today)
-                  ),)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: _birthDate,
+                    readOnly: true,
+                    decoration: InputDecoration(
+                      labelText: 'Birth Date',
+                      prefixIcon: Icon(Icons.calendar_today),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey)
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide( color: Colors.blue)
+                      ),
+                    ),
+                    onTap: (){
+                      _selectDate();
+                    } ,
+                    ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: IntlPhoneField(
+                    controller: phone,
+                    decoration: InputDecoration(
+                      labelText: 'Phone Number',
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey)
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide( color: Colors.blue)
+                        ),
+                    ),
+                    initialCountryCode: 'EG',
+                    
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: EZEmailField(
+                    controller: personalEmail,
+                    decoration: InputDecoration(
+                        labelText: 'Personal Email',
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey)
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide( color: Colors.blue)
+                          ),
+                      ),
+                    
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: DropdownButtonFormField<String>(
+                    decoration: InputDecoration(
+                          labelText: 'Academic Level',
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey)
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide( color: Colors.blue)
+                            ),
+                      ),
+                    value: _selectedValue,
+                    items: ['Prep School','Thanawy','STEM','University']
+                      .map((option) =>DropdownMenuItem<String>(
+                        value: option,
+                        child: Text(option),
+                      )).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedValue = value;
+                      });
+                    },
+                    validator: (value) {
+                      if (value == null) {
+                        return 'Please select and option';
+                      }
+                      return null ;
+                    },
+                    ),
+                )
               ],)
             ),
           ),
@@ -57,7 +163,7 @@ class _RegisterPageState extends State<RegisterPage> {
         ],
         currentStep: currentStep,
         onStepContinue: (){
-          print(firstName);
+          
           final isLastStep = currentStep == 2;
           if (isLastStep) {
             print('completed');
@@ -69,5 +175,19 @@ class _RegisterPageState extends State<RegisterPage> {
         },
       ),
     ); 
+  }
+  Future<void> _selectDate() async{
+    DateTime? _picked = await showDatePicker(
+      context: context,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2014),
+      initialDate: DateTime(2009),
+
+    );
+    if (_picked != null){
+      setState(() {
+        _birthDate.text = _picked.toString().split(" ")[0];
+      });
+    }
   }
 }
